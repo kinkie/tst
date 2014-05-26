f7282f553098a2e1c71047e99d503da76df7c0a8
Francesco Chemolli <kinkie@squid-cache.org>
2014-05-26 23:09:17 +0200

    Automated testing started implementing TernaryTrie::at()

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ CFLAGS = -g
 CXXFLAGS = -g -std=c++11
 
 all: libTernaryTrie.a TestTernaryTrie
+	./TestTernaryTrie
 
 clean:
 	rm *.a *.o
@@ -11,4 +12,3 @@ libTernaryTrie.a: TernaryTrie.o
 	
 TestTernaryTrie: TestTernaryTrie.o libTernaryTrie.a TernaryTrie.h
 	g++ $(CXXFLAGS) $^ -o $@ -L. -lTernaryTrie -lcppunit
-
--- TernaryTrie.h
+++ TernaryTrie.h
@@ -1,12 +1,13 @@
 #ifndef SQUID_TERNARYTRIE_H_
 #define SQUID_TERNARYTRIE_H_
 
+#include <stdexcept>
 #include <utility>
 
 /* for inspiration see:
  * https://github.com/nlehuen/pytst/tree/master/include
  * https://code.google.com/p/tstdb/source/browse/#svn%2Ftrunk%2Fv2%2Fsrc
- *
+ * http://algs4.cs.princeton.edu/52trie/TST.java.html
  */
 template <class Key, class Value>
 class TernaryTrie
@@ -16,13 +17,11 @@ public:
     typedef Value mapped_type;
     typedef std::pair<const Key, Value> value_type;
     typedef std::size_t size_type;
-    class Node;
-    class TernaryTrieIterator;
 
     class Node {
         friend class TernaryTrie;
         Node *left_, *right_, *equal_, *parent_;
-        TernaryTrie::value_type data;
+        TernaryTrie::value_type data; // if data.first.empty(), no data is present
         unsigned char c_;
     public:
         explicit Node ( Node *parent, unsigned char c ) : left_(nullptr), right_(nullptr), equal_(nullptr), parent_(parent), c_(c) {}
@@ -52,11 +51,12 @@ public:
     // reverse_iterator rend();
     // const_reverse_iterator crend() const;
 
-    bool empty() const;
-    size_type size() const;
-    size_type max_size() const;
+    bool empty() const { return size_ == 0; }
+    size_type size() const { return size_; }
+    size_type max_size() const; // placeholder
 
     mapped_type& operator[] (const key_type& k); //(*((this->insert(make_pair(k,mapped_type()))).first)).second
+    // returns ref to element, throws std::out_of_range if element is not present
     mapped_type& at (const key_type& k);
     const mapped_type& at (const key_type& k) const;
 
@@ -81,6 +81,9 @@ public:
     //size_type count (const key_type& k) const { return find(k)!=end(); }
 
 private:
+
+    Node *getNode(Node *subtree, const key_type &key, size_type pos);
+
     Node *root;
     size_type size_;
 };
@@ -98,6 +101,21 @@ TernaryTrie<Key, Value>::Node::~Node() {
     delete left_;
     delete right_;
     delete equal_;
+}
+
+template <class Key, class Value>
+typename TernaryTrie<Key, Value>::mapped_type& TernaryTrie<Key, Value>::at (const key_type& k)
+{
+    if (!root)
+        throw std::out_of_range("Element not found");
+    // TODO finish using getNode
+}
+
+template <class Key, class Value>
+typename TernaryTrie<Key, Value>::Node *getNode(typename TernaryTrie<Key, Value>::Node *subtree, const typename TernaryTrie<Key, Value>::key_type &key, typename TernaryTrie<Key, Value>::size_type pos )
+{
+    // TODO implement
+    return nullptr;
 }
 
 #endif /* SQUID_TERNARYTRIE_H_ */
